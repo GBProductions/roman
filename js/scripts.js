9a92d311eaeb655f049866a9c73a48064028c562
Garrett Brown <garrettpaulbrown@gmail.com>
2021-02-10 20:46:30 -0800

    Add function to check if number is within range to be converted.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,36 @@
-function romanNumerals(number) {
-    if (number === 1) {
-        return "I";
-    }   else if (number === 5) {
-        return "V";
-    }   else if (number === 10) {
-        return "X"; 
-    }   else if (number === 50) {
-        return "L";
-    }   else if (number === 100) {
-        return "C";
-    }   else if (number === 500) {
-        return "D";
-    }   else if (number === 1000) {
-        return "M";
+//Business Logic
+
+
+//Will check if number is within range to be converted into Roman Numerals.
+function isNumber(number) {
+    if ((number < 0) || (number > 3999)) {
+        return "This number cannot be converted! Pick a different one.";
+    }   else {
+        return true
     }
 }
 
+function romanNumerals(numberString) {
+    const array = numberString.split("");
+
+    console.log(array);
+}
+
+
+//User Logic
 
 $(document).ready(function() {
     $("form#number-submit").submit(function(event) {
     event.preventDefault();
-    const number = parseInt($("input#number").val());
-    const value = romanNumerals(number)
-    console.log(value)
+    const numberString = $("input#number").val();
+    const number = parseInt(numberString);
+
+    const pass = isNumber(number)
+    console.log(pass);
+
+    const value = romanNumerals(numberString);
+
+
+
     });
 });
